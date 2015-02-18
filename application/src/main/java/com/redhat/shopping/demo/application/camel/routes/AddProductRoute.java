995055212791c54df64d5890ff70ecf9f72fa24b
Ankit Verma <ankit.b.verma@accenture.com>
2015-02-18 18:05:07 +0530

    Sales force Blueprint Updated

--- application/src/main/java/com/redhat/shopping/demo/application/camel/routes/AddProductRoute.java
+++ application/src/main/java/com/redhat/shopping/demo/application/camel/routes/AddProductRoute.java
@@ -1,6 +1,7 @@
 package com.redhat.shopping.demo.application.camel.routes;
 
-import java.net.URL;
+import java.io.File;
+import java.net.URI;
 import java.util.List;
 
 import org.apache.camel.Exchange;
@@ -8,11 +9,8 @@ import org.apache.camel.Processor;
 import org.apache.camel.builder.NoErrorHandlerBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
-import org.apache.commons.io.IOUtils;
 
 public class AddProductRoute extends RouteBuilder {
-	private String productDirectorySource;
-	
 	public void configure() {
 		errorHandler(new NoErrorHandlerBuilder());
 		JaxbDataFormat dataFormat = new JaxbDataFormat("com.redhat.shopping.demo.application.pojos.jpa");
@@ -25,9 +23,9 @@ public class AddProductRoute extends RouteBuilder {
 			public void process(Exchange exchange) throws Exception {
 				String urlPath = (String)exchange.getIn().getBody(List.class).get(0);
 				if(urlPath!=null){
-					exchange.getIn().setBody(IOUtils.toByteArray((new URL(urlPath)).openStream()));
+					exchange.getIn().setBody(new File(new URI(urlPath)));
 				}else{
-				exchange.getIn().setBody("File Path Is Null");
+					exchange.getIn().setBody("File Path Is Null");
 				}
 				
 				
@@ -36,21 +34,8 @@ public class AddProductRoute extends RouteBuilder {
 		.split().xpath("/products-list/products").parallelProcessing()
 		.log("${body}")
 		.to("activemq:queue:insertProductsFromQueue")
-		.process(new Processor(){
-			public void process(Exchange exchange) throws Exception {
-				exchange.getOut().setBody("Your Request Is Being Processed".getBytes());
-			}
-		})
 		;
 		
 		
-	}
-
-	public String getProductDirectorySource() {
-		return productDirectorySource;
-	}
-
-	public void setProductDirectorySource(String productDirectorySource) {
-		this.productDirectorySource = productDirectorySource;
 	}
 }
